e489d2f21349e8b9cc7e456a549d6c07ac7380dd
Yaroslav Smoliak <yaroslav-Euristiq@octopuslabs.com>
2019-12-28 15:45:05 +0200

    Use ports

--- ViberApi/Program.cs
+++ ViberApi/Program.cs
@@ -17,8 +17,13 @@ namespace ViberApi
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:" + port);
+        }
     }
 }
